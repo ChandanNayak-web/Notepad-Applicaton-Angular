7f8cbf508b158accde2c3987edbd8374ca562310
chandan <chandan2000kulu@gmail.com>
2022-04-18 13:21:14 +0530

    i create route,model file define schema using model and define my api using router

--- backend/db.js
+++ backend/db.js
@@ -10,22 +10,5 @@ const connectToMongo = () => {
     })
 }
 
-// var connectToMongo = async () => {
-//     try {
-//       const conn = await mongoose.connect(mongoURI, {
-//         useUnifiedTopology: true,
-//         useNewUrlParser: true,
-//       })
-  
-//       console.log(`MongoDB Connected: ${conn.connection.host}`)
-//     } catch (error) {
-//       console.error(`Error: ${error.message}`)
-//       process.exit(1)
-//     }
-//   }
-
-
-
-
 
 module.exports = connectToMongo;
