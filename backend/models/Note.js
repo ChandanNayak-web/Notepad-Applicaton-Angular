44bf5a0f7f7679bee46d538b92992e566c53e1cd
chandan <chandan2000kulu@gmail.com>
2022-04-21 13:48:26 +0530

    here i add and fetch the notes

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose; //https://mongoosejs.com/docs/guide.html
+
 
 const NotesSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'user'
+    },
     title: {
         type: String,
         required: true
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,50 @@
 const express = require('express');
 const router = express.Router();
+var fetchuser = require('../middleware/fetchuser');
+const Note = require('../models/Note');
+const { body, validationResult } = require('express-validator'); //https://express-validator.github.io/docs/
 
-router.get('/', (req, res) => {
-    
-    res.json([])
+
+//Rout 1:Get all the notes using :GET "/api/auth/fetchallnotes". login require 
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+
+        const notes = await Note.find({ user: req.user.id });
+        res.json(notes)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+
+
+//Rout 2:Add a new notes using :POST "/api/auth/addnote". login require 
+router.post('/addnote', fetchuser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must be atleast 5 character').isLength({ min: 5 }),
+], async (req, res) => {
+    try {
+
+
+        const { title, description, tag } = req.body;
+        //If there are errors ,return Bad request and the errors
+        const errors = validationResult(req); //https://express-validator.github.io/docs/
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() }); // here it will return the error message
+        }
+
+
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        })
+        const savedNote = await note.save();
+
+        res.json(savedNote)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal Server Error");
+    }
 })
 
 
