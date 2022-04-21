0cf6bd436bbab245e4506760b804590aa6217145
chandan <chandan2000kulu@gmail.com>
2022-04-21 08:04:29 +0530

    here i crecte a another end point for log in it will only give the response as authtoken by the auth token we can get the id

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,7 +7,7 @@ var jwt = require('jsonwebtoken'); //https://www.npmjs.com/package/jsonwebtoken
 
 const JWT_SECRET = 'chandanisagoodb$oy';
 
-//create a user using :POST "/api/auth/createuser". Doesn't require login
+//Rout 1:create a user using :POST "/api/auth/createuser". Doesn't require login
 router.post('/createuser', [
    body('name', 'Enter a valid name').isLength({ min: 3 }),
    body('email', 'Enter a valid email').isEmail(),
@@ -48,16 +48,58 @@ router.post('/createuser', [
          }
       }
       const authtoken = jwt.sign(data, JWT_SECRET)
-      
+
 
       // res.json(user);
-      res.json({authtoken})
+      res.json({ authtoken })
    }
    //catch the error
    catch (error) {
       console.log(error.message);
-      res.status(500).send("Some error occured");
+      res.status(500).send("Internal Server Error");
    }
+})
+
+
+//Rout 2:Authenticate a user using :POST "/api/auth/login". Doesn't require login
+router.post('/login', [
+   body('email', 'Enter a valid email').isEmail(),
+   body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+
+   //If there are errors ,return Bad request and the errors
+   const errors = validationResult(req); //https://express-validator.github.io/docs/
+   if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() }); // here it will return the error message
+   }
+
+
+   const { email, password } = req.body;
+   try {
+      let user = await User.findOne({ email });
+      if (!user) {
+         return res.status(400).json({ error: "please try to login with correct credential" })
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password)
+      if (!passwordCompare) {
+         return res.status(400).json({ error: "please try to login with correct credential" })
+
+      }
+
+      const data = {
+         user: {
+            id: user.id
+         }
+      }
+      const authtoken = jwt.sign(data, JWT_SECRET)
+      res.json({ authtoken })
+   } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal Server Error");
+   }
+
+
 })
 
 
